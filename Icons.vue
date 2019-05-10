26f2f97b8403d7dba4977ab5f235f884296937c9
BlasterDinray <jyto7034@naver.com>
2019-05-10 09:55:47 +0900

    Init

---
+++ Icons.vue
@@ -0,0 +1,11 @@
+<template>
+  <svg height="100" width="100">
+    <circle cx="50" cy="50" r="40" fill="red" />
+  </svg>
+</template>
+
+<script>
+export default {
+  name: 'my-icon'
+}
+</script>
--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,35 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app dark>
+    <v-content>
+      <HelloWorld/>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld'
 
 export default {
-  name: 'app',
+  name: 'App',
   components: {
     HelloWorld
+  },
+  data () {
+    return {
+      //
+    }
   }
 }
 </script>
 
+
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
 }
+    html{
+      overflow-y: hidden;
+    }
 </style>
